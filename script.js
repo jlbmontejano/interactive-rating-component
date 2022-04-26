e7d6fb397b40bd335e00e34a9fce46a0fab20b28
Jorge Luis Buenrostro Motejano <jlbmontejano@gmail.com>
2022-04-26 13:27:17 -0700

    Update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const submitButton = document.querySelector('input');
+const submitButton = document.getElementById('submit-button');
 
-submitButton.addEventListener('click', () => alert('Hola'));
+submitButton.addEventListener('click', () => {
+    const ask = document.getElementById('asking-box');
+    const thank = document.getElementById("thank-you-box");
+    let rating = document.querySelector('input[type = radio]:checked').value;
+    
+    if(rating !== null) {
+
+        ask.style.display = "none";
+        thank.style.display = "block";
+
+        document.getElementById('final-rating').innerHTML = `You selected ${rating} out of 5`;
+    }
+});
