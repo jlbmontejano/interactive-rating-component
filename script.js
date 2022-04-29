89501327ffbff0f4d0ef2671ad3079370e3939ee
Jorge Buenrostro <jlbmontejano@gmail.com>
2022-04-28 21:41:08 -0700

    added exceptions and mouse transform on click

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const submitButton = document.getElementById('submit-button');
+const radioButtons = document.querySelectorAll('input[name="number"]');
+
+const enableSubmit = () => {
+    submitButton.classList.remove('disabled');
+    submitButton.removeAttribute('disabled');
+};
 
 submitButton.addEventListener('click', () => {
     const ask = document.getElementById('asking-box');
     const thank = document.getElementById("thank-you-box");
     let rating = document.querySelector('input[type = radio]:checked').value;
-    
-    if(rating !== null) {
+
+    if (rating !== null) {
 
         ask.style.display = "none";
         thank.style.display = "block";
